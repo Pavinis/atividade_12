06f3645f1af7ceb1505ca0d93a218d643299701e
Paulo V S <paulo-v_siqueira@estudante.sesisenai.org.br>
2024-04-15 15:36:02 -0300

    commit final

--- atividade12.js
+++ atividade12.js
@@ -1,22 +1,24 @@
 function calculando() {
     let opcao = document.querySelector("#opcao").value;
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
     let res = 0;
 
     switch (opcao) {
         case "adicao":
-            res = num1 + num2;
+            res = (num1 + num2);
+            break;
         case "subtracao":
             res = num1 - num2;
+            break;
         case "multiplicacao":
             res = num1 * num2;
+            break;
         case "divisao":
             res = num1 / num2;
-        default:
-            res = null;
+            break;
     }
-    document.getElementById('resultado').innerHTML = res;
+    document.getElementById('resultado').innerHTML = ("   " + res);
     console.log(opcao);
     console.log(num1);
     console.log(res);
