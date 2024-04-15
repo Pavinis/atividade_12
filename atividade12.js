005d1075518bf5617757cde24e48ae88c65433fc
Paulo V S <paulo-v_siqueira@estudante.sesisenai.org.br>
2024-04-15 20:10:21 -0300

    adicionando operações

--- atividade12.js
+++ atividade12.js
@@ -6,7 +6,7 @@ function calculando() {
 
     switch (opcao) {
         case "adicao":
-            res = (num1 + num2);
+            res = num1 + num2;
             break;
         case "subtracao":
             res = num1 - num2;
@@ -17,9 +17,48 @@ function calculando() {
         case "divisao":
             res = num1 / num2;
             break;
+        case "mod":
+            res = num1 % num2;
+            break;
+        case "potencia":
+            res = num1 ** num2;
+            break;
+        case "log":
+            res = Math.log10(num1);
+            document.getElementById('num2').value = (null);
+            break;
+        case "sqrt":
+            res = Math.sqrt(num1);
+            document.getElementById('num2').value = (null);
+            break;
+        case "porcentagem":
+            res = num1 / 100;
+            document.getElementById('num2').value = (null);
+            break
     }
-    document.getElementById('resultado').innerHTML = ("   " + res);
+    document.getElementById('resultado').innerHTML = (res);
     console.log(opcao);
     console.log(num1);
     console.log(res);
+}
+
+function tiraNum2() {
+    let opcao = document.querySelector("#opcao").value;
+
+    switch (opcao) {
+        case "log":
+            document.getElementById('num2').placeholder = ("Este espaço não será utilizado");
+            document.getElementById('num2').value = (null);
+            break;
+        case "sqrt":
+            document.getElementById('num2').placeholder = ("Este espaço não será utilizado");
+            document.getElementById('num2').value = (null);
+            break;
+        case "porcentagem":
+            document.getElementById('num2').placeholder = ("Este espaço não será utilizado");
+            document.getElementById('num2').value = (null);
+            break;
+        default:
+            document.getElementById('num2').placeholder = ("");
+    }
 }
